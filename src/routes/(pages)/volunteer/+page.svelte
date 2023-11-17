7a05cc41856bdf81cc1ed3e1f711dd1b2389f04d
Simon Elvery <simon@elvery.net>
2023-11-17 13:02:02 +1000

    Fix build issues and enable prerendering globally

--- src/routes/(pages)/volunteer/+page.svelte
+++ src/routes/(pages)/volunteer/+page.svelte
@@ -1,69 +1,73 @@
 <script>
-	import PageBody from '$lib/PageBody.svelte';
-	import PageHeader from '$lib/PageHeader.svelte';
-	import SectionTitle from '$lib/SectionTitle.svelte';
+  import PageBody from '$lib/PageBody.svelte';
+  import PageHeader from '$lib/PageHeader.svelte';
+  import SectionTitle from '$lib/SectionTitle.svelte';
 </script>
 
 <PageHeader>
-	<h1>Volunteering</h1>
-	<p>
-		Thanks for being part of the <strong>Hacks/Hackers Brisbane</strong>{' '}
-		community!
-	</p>
-	<p>
-		Each event that we put on takes a considerable amount of work and
-		coordination, so we're always keen to hear from people who are willing to
-		help out the core group of organisers in any way — large or small.
-	</p>
+  <h1>Volunteering</h1>
+  <p>
+    Thanks for being part of the <strong>Hacks/Hackers Brisbane</strong>{' '}
+    community!
+  </p>
+  <p>
+    Each event that we put on takes a considerable amount of work and
+    coordination, so we're always keen to hear from people who are willing to
+    help out the core group of organisers in any way — large or small.
+  </p>
 </PageHeader>
 <PageBody>
-	<SectionTitle>How to help</SectionTitle>
+  <SectionTitle>How to help</SectionTitle>
 
-	<div>
-		<p
-			>There are a variety of things we could use some help with both <a
-				href="#event-help">on the day</a>
-			of events and <a href="#organising-help">between events</a>. If you are
-			willing to help out with any of these tasks, we'd love to hear from you
-			<a
-				href="mailto:hello@quantsquill.com?subject=Hacks%2FHackers%20offer%20to%20help"
-				>via email</a
-			>.</p>
-	</div>
-	<SectionTitle name="#organising-help">Between events</SectionTitle>
-	<div>
-		<h4>Creating promotional material</h4>
-		<p
-			>Got marketing or copywriting skills? Creating event flyers and writing
-			event descriptions.</p>
-		<h4>Event writeups</h4>
-		<p
-			>We love writups of our events. We try to get a writup into our post-event <a
-				href="/newsletter">newsletter</a> and love just as much seeing writeups elsewhere.</p>
-	</div>
-	<SectionTitle name="#event-help">On the day</SectionTitle>
-	<div>
-		<h4>Venue setup</h4>
-		<p
-			>Putting out chairs and setting up tables for the food and drinks. Setting
-			up the projector, etc.</p>
-		<h4>Photographer</h4>
-		<p
-			>If you're a photography pro (or aspiring to be one) having great photos
-			of our events makes a huge difference.</p>
-		<h4>Greeter</h4>
-		<p
-			>This involves checking attendees off against the RSVP list. Bonus — you
-			get to say hello to everyone there!</p>
-		<h4>Cammo</h4>
-		<p>Do you have sound/video recording skills (or a desire to build them?)</p>
-		<h4>Live tweeting</h4>
-		<p
-			>Live tweet the event either from your own account, or we can give you
-			access to @HacksHackersBrisbane for the night.</p>
-		<h4>Packing up</h4>
-		<p
-			>Many hands make light work when packing up at the end of an event. If you
-			can hang around for a bit that would be awesome.</p>
-	</div>
-</PageBody>
+  <div>
+    <p
+      >There are a variety of things we could use some help with both <a
+        href="#event-help">on the day</a>
+      of events and <a href="#organising-help">between events</a>. If you are
+      willing to help out with any of these tasks, we'd love to hear from you
+      <a
+        href="mailto:hello@quantsquill.com?subject=Hacks%2FHackers%20offer%20to%20help"
+        >via email</a
+      >.</p>
+  </div>
+  <div id="organising-help">
+    <SectionTitle>Between events</SectionTitle>
+    <div>
+      <h4>Creating promotional material</h4>
+      <p
+        >Got marketing or copywriting skills? Creating event flyers and writing
+        event descriptions.</p>
+      <h4>Event writeups</h4>
+      <p
+        >We love writups of our events. We try to get a writup into our
+        post-event <a href="/newsletter">newsletter</a> and love just as much seeing
+        writeups elsewhere.</p>
+    </div>
+    <div id="event-help" />
+    <SectionTitle>On the day</SectionTitle>
+    <div>
+      <h4>Venue setup</h4>
+      <p
+        >Putting out chairs and setting up tables for the food and drinks.
+        Setting up the projector, etc.</p>
+      <h4>Photographer</h4>
+      <p
+        >If you're a photography pro (or aspiring to be one) having great photos
+        of our events makes a huge difference.</p>
+      <h4>Greeter</h4>
+      <p
+        >This involves checking attendees off against the RSVP list. Bonus — you
+        get to say hello to everyone there!</p>
+      <h4>Cammo</h4>
+      <p
+        >Do you have sound/video recording skills (or a desire to build them?)</p>
+      <h4>Live tweeting</h4>
+      <p
+        >Live tweet the event either from your own account, or we can give you
+        access to @HacksHackersBrisbane for the night.</p>
+      <h4>Packing up</h4>
+      <p
+        >Many hands make light work when packing up at the end of an event. If
+        you can hang around for a bit that would be awesome.</p>
+    </div>
+  </div></PageBody>
